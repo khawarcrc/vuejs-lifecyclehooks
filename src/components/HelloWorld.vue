46e69047dc5bc2169d2542666a2baf6ba063b031
khawarcrc <khawarcrc@gmail.com>
2023-10-23 23:08:32 -0700

    prop data and methods in vue and there purpose

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- Display the value of the 'msg' prop in an <h1> element -->
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <!-- Display a button and bind a click event to the 'incrementCount' method -->
+    <button @click="count++">Incrememnt</button>
+    <span>{{ count }}</span>
+    <button @click="decrementCount">Decrement</button>
+    <!-- Display the current value of the 'count' variable -->
+  </div>
+</template>
+
+<script>
+export default {
+  // Define the component and specify its props
+  props:['msg',],
+  // props: {
+  //   msg: String,
+  // },
+  // Define the component's data using the 'data' option
+  data() {
+    return {
+      count: 0, // Initialize the 'count' variable with a value of 0
+    };
+  },
+
+  // Define the component's methods
+  methods: {
+    // Define a method called 'incrementCount'
+    incrementCount() {
+      this.count++; // Increment the 'count' variable by 1
+    },
+    decrementCount() {
+      this.count--;
+    },
+  },
+};
+</script>
